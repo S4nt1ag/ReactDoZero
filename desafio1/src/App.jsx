e6d533ac6d0987b390f3a19782a655daccd09add
Santiago <127254370+S4nt1ag@users.noreply.github.com>
2024-01-04 19:40:18 -0300

    desafio1

---
+++ desafio1/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>Primerio exercicio</h1>
+      <p>esse desafio é escrever algo e personalizar esse algo</p>
+    </div>
+  )
+}
+
+export default App
